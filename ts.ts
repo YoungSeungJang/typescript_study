f48692808afd36ff73e5d34074884135c01c14d9
YoungSeungJang <1345620@naver.com>
2023-07-16 23:20:38 +0900

    :sparkles:Feat: ts

--- ts.ts
+++ ts.ts
@@ -8,3 +8,15 @@ any
 */
 
 let a: number = 3;
+
+// 에러뜬다
+// a='123'
+
+let b: string = 'love you';
+
+let c: any = 4;
+c = 'asdfasd';
+// 에러가 나지않음 any기 때문에
+// but any를 많이쓰는건 자제를 해야한다 => 이걸쓰면 typescript를 쓰는 이유가 없는데??
+// 아직 타입이 정해지지 않았을 경우, 타입이 변하는 경우만 한정적으로 사용, 남발 X
+
