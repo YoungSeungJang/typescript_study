f3431eec197d21448bf43d56533f74e3ab18acf0
YoungSeungJang <1345620@naver.com>
2023-07-17 23:10:39 +0900

    :sparkles:Feat: ts

--- ts.ts
+++ ts.ts
@@ -20,3 +20,12 @@ c = 'asdfasd';
 // but any를 많이쓰는건 자제를 해야한다 => 이걸쓰면 typescript를 쓰는 이유가 없는데??
 // 아직 타입이 정해지지 않았을 경우, 타입이 변하는 경우만 한정적으로 사용, 남발 X
 
+let d: string | number = 'asdafsd';
+d = 3;
+d = null;
+// ??왜 에러가 안뜨지
+console.log(d);
+// 타입을 여러개 설정도 가능!
+
+let e: string[] = ['apple', 'mongo'];
+let f: number[] = [1, 2];
